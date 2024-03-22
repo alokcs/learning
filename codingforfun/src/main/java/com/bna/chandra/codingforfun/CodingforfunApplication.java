1e303d1a5f26b6b0b3586b52cb9bbb9d6345f39c
alokcs <alokcs@yahoo.com>
2024-03-21 23:11:12 -0500

    git initial commit

---
+++ codingforfun/src/main/java/com/bna/chandra/codingforfun/CodingforfunApplication.java
@@ -0,0 +1,14 @@
+package com.bna.chandra.codingforfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingforfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingforfunApplication.class, args);
+		System.err.println("Hello Alok");
+	}
+
+}
---
+++ codingforfun/src/test/java/com/bna/chandra/codingforfun/CodingforfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bna.chandra.codingforfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingforfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
